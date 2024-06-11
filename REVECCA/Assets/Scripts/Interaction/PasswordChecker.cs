00160137bc619e39878240d7ecb900ecfb6f22c8
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-06-11 16:41:16 +0900

    RVECCA_Password
    패스워드 inputFiled 활용하여 수정

--- REVECCA/Assets/Scripts/Interaction/PasswordChecker.cs
+++ REVECCA/Assets/Scripts/Interaction/PasswordChecker.cs
@@ -1,100 +1,4 @@
 /*using UnityEngine;
-using TMPro;
-
-public class PasswordChecker : MonoBehaviour
-{
-    public TextMeshProUGUI resultText; // 결과를 출력하는 Text
-    public TextMeshProUGUI inputDisplayText; // 사용자 입력을 실시간으로 보여주는 Text
-    public GameObject password;
-
-    private string inputPassword = ""; // 사용자가 입력한 암호
-    private string correctPassword = "1234"; // 올바른 암호
-
-    private void Start()
-    {
-        password.SetActive(false);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            password.SetActive(true);
-            inputPassword = ""; // 플레이어가 트리거 영역에 들어올 때마다 암호 입력 초기화
-            UpdateDisplay(); // 입력 디스플레이 업데이트
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        password.SetActive(false);
-    }
-
-    void Update()
-    {
-        // 입력 처리 로직을 Update 메서드로 이동
-        HandleInput();
-    }
-
-    void HandleInput()
-    {
-        foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
-        {
-            if (Input.GetKeyDown(keyCode))
-            {
-                // 숫자 키 입력 감지
-                if (keyCode >= KeyCode.Alpha0 && keyCode <= KeyCode.Alpha9)
-                {
-                    inputPassword += (char)('0' + (keyCode - KeyCode.Alpha0));
-                }
-                else if (keyCode >= KeyCode.Keypad0 && keyCode <= KeyCode.Keypad9)
-                {
-                    inputPassword += (char)('0' + (keyCode - KeyCode.Keypad0));
-                }
-
-                UpdateDisplay(); // 입력 디스플레이 업데이트
-            }
-        }
-
-        // Backspace 키를 누를 경우 마지막 숫자 삭제
-        if (Input.GetKeyDown(KeyCode.Backspace) && inputPassword.Length > 0)
-        {
-            inputPassword = inputPassword.Substring(0, inputPassword.Length - 1);
-            UpdateDisplay(); // 입력 디스플레이 업데이트
-        }
-
-        // Enter 또는 Return 키를 누를 경우 암호 확인
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            CheckPassword();
-            inputPassword = ""; // 암호 입력 초기화
-            UpdateDisplay(); // 입력 디스플레이 업데이트
-        }
-    }
-
-    void CheckPassword()
-    {
-        // 암호를 확인하고 결과를 출력
-        if (inputPassword == correctPassword)
-        {
-            resultText.text = "암호가 정확합니다!";
-            resultText.color = Color.green;
-        }
-        else if (inputPassword.Length >= correctPassword.Length)
-        {
-            resultText.text = "암호가 틀렸습니다.";
-            resultText.color = Color.red;
-        }
-    }
-
-    void UpdateDisplay()
-    {
-        // 입력된 암호를 디스플레이에 표시
-        inputDisplayText.text = inputPassword;
-    }
-}
-*/
-using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
@@ -210,5 +114,99 @@ public class PasswordChecker : Interactable
         // 퀘스트 완료 여부를 확인하는 로직을 구현하세요
         // 예를 들어:
         return isQuestCompleted; 
+    }
+}*/
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PasswordChecker : Interactable
+{
+    public TextMeshProUGUI resultText;
+    public TMP_InputField inputPassword;
+    public GameObject successObject;
+
+    [SerializeField]
+    private string correctPassword = "1234";
+
+    protected override void Start()
+    {
+        dialogueText = null;
+        base.Start();
+        inputPassword.characterLimit = 4; // 4자리로 제한
+        //inputPassword.contentType = TMP_inputPassword.ContentType.IntegerNumber; // 숫자만 입력 가능하도록 설정
+        inputPassword.onValueChanged.AddListener(OnPasswordChanged);
+        inputPassword.onEndEdit.AddListener(OnPasswordEntered);
+
+    }
+
+    protected override void OnInteract()
+    {
+        isQuestCompleted = true;
+        inputPassword.gameObject.SetActive(true);
+    }
+
+    protected override void OnExit()
+    {
+        isQuestCompleted = false;
+        inputPassword.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        // npcObject가 null이 아닌 경우에만 F키 입력을 확인
+        if (isQuestCompleted && Input.GetKeyDown(KeyCode.F))
+        {
+            GameManager.instance.ItemOff();
+            isGetInteraction = true;
+            Debug.Log(isGetInteraction);
+            if (Input.GetKeyDown(KeyCode.F) && isGetInteraction)
+            {
+                interactableObject.SetActive(true);
+                inputPassword.ActivateInputField(); // 입력 필드 활성화
+            }
+        }
+    }
+
+    public void OnPasswordChanged(string password)
+    {
+        Debug.Log("암호 입력 중");
+        // 입력된 암호가 4자리를 넘지 않도록 제한
+        if (password.Length > 4)
+        {
+            inputPassword.text = password.Substring(0, 4);
+        }
+    }
+
+    public void OnPasswordEntered(string password)
+    {
+        Debug.Log("암호 입력 중");
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            CheckPassword(password);
+        }
+    }
+
+    void CheckPassword(string password)
+    {
+        // 암호 확인 로직
+        if (password == correctPassword && isQuestCompleted)
+        {
+            resultText.text = "암호가 정확합니다!";
+            resultText.color = Color.green;
+            successObject.SetActive(false);
+            ProceedQuest();
+        }
+        else
+        {
+            resultText.text = "암호가 틀렸습니다.";
+            resultText.color = Color.red;
+        }
+    }
+
+    protected override bool CheckQuestCompletion()
+    {
+        // 퀘스트 완료 여부를 확인하는 로직을 구현하세요
+        // 예를 들어:
+        return isQuestCompleted;
     }
 }
